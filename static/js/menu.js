d4c8966c485ccef8ccaaed862fdc7b093422cc9f
Delta12348 <manusandoval05@gmail.com>
2021-11-15 19:02:49 -0600

    Basic responsive site

--- static/js/menu.js
+++ static/js/menu.js
@@ -4,24 +4,4 @@ function menuToggle(){
 
     toggleMenu.classList.toggle("active");
     menu.classList.toggle("active");
-    if(toggleMenu.classList.contains("active")){
-        menu.style.zIndex = 6; 
-        const icon = document.createElement("img"); 
-        icon.src = ""; 
-        icon.alt = "Close Menu icon"; 
-        icon.classList.add("icon");
-
-        document.querySelector(".icon").remove();
-        toggleMenu.append(icon); 
-    }
-    else if(!toggleMenu.classList.contains("active")){
-        section.style.zIndex = 5; 
-        const icon = document.createElement("img");
-        icon.src = ""; 
-        icon.alt = "Open Menu Button"; 
-        icon.classList.add("icon"); 
-
-        document.querySelector(".icon").remove(); 
-        toggleMenu.append(icon); 
-    }
 }
